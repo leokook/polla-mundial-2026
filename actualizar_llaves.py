59c3337e04adb411a30438b2fdd98ce887ca3f1a
leokook <140652771+leokook@users.noreply.github.com>
2026-07-06 09:18:37 -0400

    1/4 teams

--- actualizar_llaves.py
+++ actualizar_llaves.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     actualizar_partido(96, "Switzerland", "Colombia")
     actualizar_partido(97, "France", "Morocco")
     #actualizar_partido(98, "yyyyy", "xxx")
-    #actualizar_partido(99, "yyyyy", "xxx")
+    actualizar_partido(99, "Norway", "England")
     #actualizar_partido(100, "yyyyy", "xxx")
     #actualizar_partido(101, "yyyyy", "xxx")
     #actualizar_partido(102, "yyyyy", "xxx")
